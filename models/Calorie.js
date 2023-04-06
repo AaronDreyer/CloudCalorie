6d7bd6063aa31cf386fc6781df895b10fae26657
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-04-05 20:55:32 -0400

    added code to calorie.js in models folder

--- models/Calorie.js
+++ models/Calorie.js
@@ -1,2 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Calorie extends Model {}
+
+Calorie.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    meal_type:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    meal: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    number_of_calories: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'CloudCalorie',
+  }
+);
+
+module.exports = Calorie;
