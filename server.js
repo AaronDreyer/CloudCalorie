493cb70a435878fa978efe2680879aec63d1812d
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-04-10 22:09:09 -0400

    added handlebars and home routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -27,7 +29,8 @@ const sess = {
 };
 
 app.use(session(sess));
-
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the public folder
