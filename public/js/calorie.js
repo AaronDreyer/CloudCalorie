c29224d0ae371cf521a02c3fd55413affe63df0f
AaronDreyer <aaronottaway@gmail.com>
2023-04-13 12:12:36 -0400

    CalorieCounterFucntion
    I manager to get the calorie counter to function but only if the website is refreshed

--- public/js/calorie.js
+++ public/js/calorie.js
@@ -1,33 +1,64 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
 
-  const meal_type = document.querySelector('#meal-type').value.trim();
-  const meal = document.querySelector('#meal-name').value.trim();
-  const number_of_calories = document.querySelector('#calories').value.trim();
+// get elements
+const mealTypeInput = document.getElementById('meal-type');
+const mealNameInput = document.getElementById('meal-name');
+const caloriesInput = document.getElementById('calories');
+const submitBtn = document.getElementById('submit-btn');
+const logBody = document.getElementById('log-body');
+const calorieCount = document.getElementById('calorie-count');
 
-  // Debugging: Log the values of the input fields
-  console.log('mealType:', meal_type);
-  console.log('meal:', meal);
-  console.log('numberOfCalories:', number_of_calories);
+// function to add a new row to the calorie log table
+function addRow(meal_type, meal, number_of_calories) {
+  const newRow = logBody.insertRow();
+  const mealTypeCell = newRow.insertCell(0);
+  const mealNameCell = newRow.insertCell(1);
+  const caloriesCell = newRow.insertCell(2);
+  mealTypeCell.innerHTML = meal_type;
+  mealNameCell.innerHTML = meal;
+  caloriesCell.innerHTML = number_of_calories;
+}
 
-  // Create the calorie object
-  // Debugging: Log the calorie object
+// function to update the calorie counter
+function updateCalorieCount() {
+  fetch('/api/calories')
+    .then(response => response.json())
+    .then(data => {
+      const totalCalories = data.reduce((total, log) => total + parseInt(log.number_of_calories), 0);
+      calorieCount.innerHTML = totalCalories;
+    })
+    .catch(error => console.error(error));
+}
 
-  if (meal_type && meal && number_of_calories) {
-    const response = await fetch('/api/calories', {
-      method: 'POST',
-      body: JSON.stringify({ meal_type, meal, number_of_calories }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+// function to handle form submissions
+function handleSubmit(e) {
+  e.preventDefault();
+  // get values from inputs
+  const meal_type = mealTypeInput.value;
+  const meal = mealNameInput.value;
+  const number_of_calories = caloriesInput.value;
+  // add row to table
+  addRow(meal_type, meal, number_of_calories);
+  // update calorie count
+  const formData = { meal_type, meal, number_of_calories };
+  fetch('/api/calories', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Data saved successfully:', data);
+    })
+    .catch(error => console.error(error));
+  // clear inputs
+  mealNameInput.value = '';
+  caloriesInput.value = '';
+}
 
-    if (response.ok) {
-      document.location.reload(); // Update page or redirect as needed
-    } else {
-      alert('Failed to save calorie information');
-    }
-  }
-};
+// add event listener to submit button
+submitBtn.addEventListener('click', handleSubmit);
 
-document.querySelector('#calorie-form').addEventListener('submit', newFormHandler);
+// update calorie counter on page load
+updateCalorieCount();
